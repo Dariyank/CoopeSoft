02280236e963db2f0cc6781a96f870a736cf0d2c
Dariyank Sanquintin R. <85773256+Dariyank@users.noreply.github.com>
2024-10-12 19:56:46 -0400

    Add files via upload
    Adding files

---
+++ Codigo/scripts.js
@@ -0,0 +1,11 @@
+// Función para sumar dos números
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+// Ejemplo de uso
+const numero1 = 5;
+const numero2 = 10;
+const resultado = sumar(numero1, numero2);
+
+console.log("La suma de", numero1, "y", numero2, "es:", resultado);
---
+++ README.md
@@ -0,0 +1,4 @@
+# Grupo 2 - Sistema de control de Cooperativas COOPESOFT
+
+**Descripcion:**
+Este proyecto fue realizado por los miembros del grupo.
